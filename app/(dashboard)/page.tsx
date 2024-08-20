7f7f125ec2c1be1821d1a688a10713aff8093408
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-08-21 01:48:38 +0530

    modified:   app/(dashboard)/page.tsx 	modified:   package.json

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,6 @@
 // pages/index.tsx
 import React from 'react'
-import NewsList from '../../components/NewsList'
-import ResourceList from '@/components/ResoucesList'
 import Hero from '@/components/home/Hero'
-import BlogGrid from '@/components/home/BlogGrid'
 import NewsGrid from '@/components/home/NewsGrid'
 import ResourceGrid from '@/components/home/ResourceGrid'
 
